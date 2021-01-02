908e2fad85c5bbdf147797eb0084cd0292f5184e
PF94 <45898787+flickPF94@users.noreply.github.com>
2021-01-01 22:14:38 -0500

    goddammit this is hard

--- BlocklandManager/Main.Designer.cs
+++ BlocklandManager/Main.Designer.cs
@@ -32,6 +32,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
             this.version = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // webBrowser1
@@ -58,19 +59,32 @@
             // 
             // version
             // 
+            this.version.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.version.BackColor = System.Drawing.Color.Gray;
             this.version.Font = new System.Drawing.Font("Segoe UI", 12F);
             this.version.ForeColor = System.Drawing.Color.Black;
             this.version.Location = new System.Drawing.Point(628, 41);
             this.version.Name = "version";
-            this.version.Size = new System.Drawing.Size(138, 138);
+            this.version.Size = new System.Drawing.Size(203, 60);
             this.version.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(628, 107);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(64, 22);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Play";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(834, 476);
+            this.ClientSize = new System.Drawing.Size(843, 476);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.version);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.webBrowser1);
@@ -86,6 +100,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
         public System.Windows.Forms.Panel version;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- BlocklandManager/Main.cs
+++ BlocklandManager/Main.cs
@@ -13,15 +13,24 @@ namespace BlocklandManager
 {
     public partial class Main : Form
     {
-        private readonly Image explorerIcon = Icon.ExtractAssociatedIcon(statusIcon).ToBitmap();
+        //private readonly Image explorerIcon = Icon.ExtractAssociatedIcon(statusIcon).ToBitmap();
 
+        public string blocklandExecutable = ""; //This is for the "play" button.
         public string nameDirectory = "";
         public string statusText = "Not loaded";
-        public static string statusIcon = @"C:\Windows\explorer.exe";
+        //public static string statusIcon = @"C:\Windows\explorer.exe";
+        bool isB4v21 = false; //This is set to true when "blocklandv20.exe" is detected.
         public Main()
         {
             InitializeComponent();
         }
+        /// <summary>
+        /// Makes the user select a file, and shittily detects if there's a Blockland installation or not.
+        /// This is heavily fucked, because non-v21 clients will be treated as v21 clients, which
+        /// will fuck up versions with the old addon system (v1.0x and v8).
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button1_Click(object sender, EventArgs e)
         {
             if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
@@ -30,7 +39,8 @@ namespace BlocklandManager
             if (File.Exists(curFile)) // Checks if the folder has the Blockland executable.
                 Debug.WriteLine("Checking file");
             {
-                if (File.Exists(nameDirectory + "\\glu2d3d.dll")) // Checks if the folder has a file only available on v0002 installations.
+                if (File.Exists(nameDirectory + "\\glu2d3d.dll")) // Checks if the folder has a file only available on v0002 installations. 
+                    //This file is on the B4v21 launcher, which fools BLManager into thinking it's v0002, and thus gives an unrelated error.
                 {
                     MessageBox.Show("This is a Blockland Vanilla installation. Support for Vanilla (v0002) is currently not implemented.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                     Debug.WriteLine("ERROR - TOO OLD");
@@ -39,25 +49,25 @@ namespace BlocklandManager
                 {
                     if (File.Exists(nameDirectory + "\\Blockland.exe")) // Checks if the folder has the Blockland executable.
                     {
-                        nameDirectory += "\\Blockland.exe";
-                        statusIcon = nameDirectory;
-                        this.webBrowser1.Navigate(folderBrowserDialog.SelectedPath);
+                        blocklandExecutable = nameDirectory += "\\Blockland.exe";
                         Debug.WriteLine("Success!");
                         statusText = "Blockland";
-                        Explorer_PaintAlt(version);
-                        Console.WriteLine(statusText);
+                        Console.WriteLine("Status Text: " + statusText);
+                        isB4v21 = false;
+                        Console.WriteLine("B4v21 Client?: " + isB4v21);
+                        InitAfterLoad();
                     }
                     else
                     {
                         if (File.Exists(nameDirectory + "\\blocklandv20.exe")) // Checks if the folder has the B4v21 v20 executable.
                         {
-                            nameDirectory += "\\blocklandv20.exe";
-                            statusIcon = nameDirectory;
-                            this.webBrowser1.Navigate(folderBrowserDialog.SelectedPath);
+                            blocklandExecutable = nameDirectory += "\\blocklandv20.exe";
                             Debug.WriteLine("Success! - B4v21 Client");
                             statusText = "Blockland v20";
-                            Explorer_PaintAlt(version);
-                            Console.WriteLine(statusText);
+                            Console.WriteLine("Status Text: " + statusText);
+                            isB4v21 = true;
+                            Console.WriteLine("B4v21 Client?: " + isB4v21);
+                            InitAfterLoad();
                         }
                         else
                         MessageBox.Show("This is not a Blockland installation.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
@@ -67,16 +77,30 @@ namespace BlocklandManager
 
         }
 
-        private void Explorer_Paint(object sender, PaintEventArgs e)
+        /// <summary>
+        /// Does the same commands for when a v21 client or a B4v21 v20 client is loaded,
+        /// </summary>
+        private void InitAfterLoad() 
         {
-            e.Graphics.DrawImage(explorerIcon, 15, 15); //Change position if you want.
-            e.Graphics.DrawString(statusText, version.Font, new SolidBrush(version.ForeColor), new RectangleF(15, 84, 100, 0));
+            this.webBrowser1.Navigate(folderBrowserDialog.SelectedPath);
+            Explorer_PaintAlt(version);
+            if (isB4v21)
+            {
+                Console.WriteLine("i is less than j");
+            }
+
         }
 
+        /// <summary>
+        /// Draws the text for the grey box. This code is a port from ms2Start, which had a heavily simplified version
+        /// of TweakUtility's icon loader helper.
+        /// </summary>
+        /// <param name="sender"></param>
         private void Explorer_PaintAlt(object sender)
         {
-            version.CreateGraphics().DrawImage(explorerIcon, 15, 15); //Change position if you want.
-            version.CreateGraphics().DrawString(statusText, version.Font, new SolidBrush(version.ForeColor), new RectangleF(15, 84, 100, 0));
+            //version.CreateGraphics().DrawImage(explorerIcon, 15, 15); //Change position if you want.
+            version.CreateGraphics().DrawString(statusText, version.Font, new SolidBrush(version.ForeColor), new RectangleF(15, 10, 185, 0));
+            //                                                                                                              X  Y  Width Height
         }
     }
     }
